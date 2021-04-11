efe4c2810e293c2a1f2ae105a4494a7381983221
ferAsam <ferasam88@gmail.com>
2021-04-11 02:06:24 -0300

    terminados tests de comercioCorresponsal

--- src/test/kotlin/ar/edu/unahur/obj2/vendedores/VendedorTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/vendedores/VendedorTest.kt
@@ -6,17 +6,24 @@ import io.kotest.matchers.booleans.shouldBeTrue
 
 class VendedorTest : DescribeSpec({
   val misiones = Provincia(1300000)
+  val cordoba = Provincia(2000000)
+  val santaFe = Provincia(10000000)
+  val laPampa = Provincia(5000000)
   val sanIgnacio = Ciudad(misiones)
+  val villaDolores = Ciudad(cordoba)
+  val rosario = Ciudad(santaFe)
+  val staRosa = Ciudad(laPampa)
+  val obera = Ciudad(misiones)
+  val esperanza = Ciudad(santaFe)
+  val sunchales = Ciudad(santaFe)
   val certificacionDeProductoFull = Certificacion(true, 100)
   val certificacionDeProductoPro = Certificacion(true, 75)
-  val certificacionDeProductoStandard = Certificacion(true, 50)
   val certificacionDeProductoBaja = Certificacion(true, 15)
   val certificacionBuena = Certificacion(false, 40)
   val certificacionMedia = Certificacion(false, 25)
   val certificacionBaja = Certificacion(false, 10)
 
   describe("Vendedor fijo") {
-    val obera = Ciudad(misiones)
     val vendedorFijo = VendedorFijo(obera)
 
     describe("puedeTrabajarEn") {
@@ -94,10 +101,6 @@ class VendedorTest : DescribeSpec({
   }
 
   describe("Viajante") {
-    val cordoba = Provincia(2000000)
-    val santaFe = Provincia(10000000)
-    val rosario = Ciudad(santaFe)
-    val villaDolores = Ciudad(cordoba)
     val viajante = Viajante(listOf(misiones))
     val viajanteInfluyente = Viajante(listOf(misiones, santaFe))
 
@@ -115,6 +118,31 @@ class VendedorTest : DescribeSpec({
       }
       it("Suma poblacion de provincias habilitadas mayor a 10000000") {
         viajanteInfluyente.esInfluyente().shouldBeTrue()
+      }
+    }
+  }
+  describe("comercioCorresponsal") {
+    val comercioCorresponsal = ComercioCorresponsal(listOf(staRosa, obera, sanIgnacio))
+    val comercioCorresponsalInfluyenteProvincia = ComercioCorresponsal(listOf(rosario, villaDolores, obera))
+    val comercioCorresponsalInfluyenteCiudad = ComercioCorresponsal(listOf(rosario, obera, esperanza, sunchales, sanIgnacio))
+
+    describe("puedeTrabajarEn") {
+      it("ciudad en la que tiene sucursal") {
+        comercioCorresponsal.puedeTrabajarEn(sanIgnacio).shouldBeTrue()
+      }
+      it("ciudad en la que no tiene sucursal") {
+        comercioCorresponsal.puedeTrabajarEn(villaDolores).shouldBeFalse()
+      }
+    }
+    describe("esInfluyente") {
+      it("sucursales en 3 provincias") {
+        comercioCorresponsalInfluyenteProvincia.esInfluyente().shouldBeTrue()
+      }
+      it("sucursales en 5 ciudades") {
+        comercioCorresponsalInfluyenteCiudad.esInfluyente().shouldBeTrue()
+      }
+      it("menos de 3 provincias con sucursales y menos de 5 ciudades con sucursales") {
+        comercioCorresponsal.esInfluyente().shouldBeFalse()
       }
     }
   }
